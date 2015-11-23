142a4a9bab6a16469318000884510961a7fe14eb
rac01 <ralf.ackermann@sap.com>
2015-11-23 18:57:29 +0100

    typo corrected

--- src/examples/python/binary_transfer/template-config.py
+++ src/examples/python/binary_transfer/template-config.py
@@ -2,7 +2,7 @@ print("Please configure appropriately and then remove this line !"); exit();
 
 # ===== Your specific configuration goes below / please adapt ========
 
-host='you_hcp_accout_id.hanatrial.ondemand.com'
+host='your_hcp_accout_id.hanatrial.ondemand.com'
 device_id='configure in IoT Cockpit'
 oauth_token='configure in IoT Cockpit'
 credentials='account_username:account_password'
