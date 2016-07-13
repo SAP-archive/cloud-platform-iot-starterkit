f517703bb7b2ae1ff0343fdf88f9bcaabf2172c7
Anton Levin <anton.levin@sap.com>
2016-07-13 11:33:39 +0200

    introduce manifest.json
    Change-Id: Iaa6e384321ebb5e41cba26b1224dc7edd996fbf5

--- src/apps/java/consumption/com.sap.iot.starterkit.ui/src/main/webapp/js/Component.js
+++ src/apps/java/consumption/com.sap.iot.starterkit.ui/src/main/webapp/js/Component.js
@@ -1,51 +1,12 @@
 sap.ui.core.UIComponent.extend( 'iotstarterkit.Component', {
 
 	metadata: {
-		routing: {
-			config: {
-				viewType: "JS",
-				viewPath: "js.view",
-				controlId: "iotstarterkitapp",
-				async: true
-			},
-			routes: [ {
-				pattern: "",
-				name: "main",
-				target: [ "main" ]
-			} ],
-			targets: {
-				main: {
-					viewName: "main",
-					viewId: "main",
-					viewLevel: 1,
-					controlAggregation: "pages"
-				}
-			}
-		}
+		manifest: "json"
 	},
 
 	init: function() {
 		sap.ui.core.UIComponent.prototype.init.apply( this, arguments );
-		this.initResourceModel();
 		this.getRouter().initialize();
-	},
-
-	initResourceModel: function() {
-		var sUrl = "locale/i18n.properties";
-		var oResourceModel = new sap.ui.model.resource.ResourceModel( {
-			bundleUrl: sUrl
-		} );
-		this.setModel( oResourceModel, "i18n" );
-	},
-
-	createContent: function() {
-		return sap.ui.view( {
-			viewName: "js.view.app",
-			type: "JS",
-			viewData: {
-				component: this
-			}
-		} );
 	}
 
 } );
